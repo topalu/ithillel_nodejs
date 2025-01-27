aef31c0f020aaaa9bb2fb2e420e6b5978000bca9
Topalu <eug.tpl@gmail.com>
2025-01-27 22:22:36 +0200

    Lesson 4

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ import Logger from "./logger/logger.js";
 
 const logger  = new Logger()
 
-logger.info("Info message")
-logger.warning("Warning message")
-logger.error("Error message")
-logger.error(new Error(''))
+// logger.info("Info message")
+// logger.warning("Warning message")
+// logger.error("Error message")
+// logger.error(new Error(''))
 
-console.log({
-    APP_ENV: process.env['APP_ENV'],
-    DB_PASS: process.env['DB_PASS'],
-    // PID: process.pid // required row
-})
+// console.log({
+//     APP_ENV: process.env['APP_ENV'],
+//     DB_PASS: process.env['DB_PASS'],
+//     // PID: process.pid // required row
+// })
 
-const p = process.env
+// const p = process.env
 
+// setInterval(() => {
+//     const timestemp = new Date().toISOString()
 
-setInterval(() => {
-    const timestemp = new Date().toISOString()
-
-    logger.info(`${timestemp}`)
-}, 10_000)
+//     logger.info(`${timestemp}`)
+// }, 10_000)
 
 // process.on("SIGINT", () => { // CTR + C
 //     console.log("SIGINT terminate ....")
@@ -52,3 +51,81 @@ process.on('unhandledRejection', (err) => {
 
     process.exit(1)
 })
+
+
+logger.info("Info message")
+
+setTimeout( () => {
+    logger.info("setTimeout 0")
+
+}, 0)
+
+setTimeout( () => {
+    logger.info("setTimeout 2000")
+
+}, 2_000);
+
+logger.info("Info message 2")
+
+const p = new Promise((res, rej) => {
+    res(1)
+});
+
+const p2 = new Promise((res, rej) => {
+    
+    setTimeout( () => {
+        res(12)
+    }, 1000);
+    logger.info("inner Promise")
+});
+
+Promise.race([p, p2])
+.then(res => {
+    console.log("all", res )
+})
+.catch(ref => {
+    console.log("ref all", ref )
+})
+
+// p.then(res => {
+//     logger.info("Promise res")
+// })
+// .catch(err => {
+//     logger.info("Promise rej")
+// })
+
+// logger.info("Info message 3")
+
+// p2.then(res => {
+//     logger.info("Promise2 res")
+// })
+// .catch(err => {
+//     logger.info("Promise2 rej")
+// })
+
+// setImmediate(res => {
+//     logger.info("setImmediate")
+// })
+
+// logger.info("Info message last")
+
+// process.nextTick(() => {
+//     logger.info("nextTick")
+// })
+
+// logger.info("now")
+
+//  await sleep(10_000)
+
+// logger.info("after 10 sec")
+
+// const timer = setInterval(() => {
+//     logger.info("setInterval")
+// }, 1000)
+
+// setTimeout(() => {
+//     clearInterval(timer)
+// }, 5000)
+
+
+
