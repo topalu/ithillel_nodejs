e7cc9847d46c7b4bd53d85d03dda714b45544985
topalu <137411168+topalu@users.noreply.github.com>
2025-01-27 20:46:21 +0200

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,8 @@
 
 export const LOCAL_ENV = 'local'
 export const PROD_ENV  = 'prod';
+export const DEV_ENV  = 'dev';
 
 export function isEnv (envStr){
-    return [LOCAL_ENV, PROD_ENV].includes(envStr)
+    return [LOCAL_ENV, PROD_ENV, DEV_ENV].includes(envStr)
 }
