b26daca1f2ce49dffe4c593179659053faa09484
Yevhen Topalu <ytopalu@foundationmedicine.com>
2025-02-13 21:16:17 +0200

    Refactor HTTP server to use Express and simplify request handling

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-import http from "node:http"
-import https from "node:https"
-import http2 from "node:http2"
-import fs from "node:fs"
-import {parse} from "querystring"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
-import { hostname } from "node:os"
 import Logger from "./logger/logger.js"
 import {getUser} from "./src/services/user.service.js"
-import express from "express"
+import express, {json} from "express"
 
 const __filname = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filname)
@@ -20,153 +14,15 @@ global.__dirname = __dirname
 
 const APPP_PORT = 3000
 
+const server = express()
 
-const server = http.createServer()
+server.use(json())
 
-server.on("request", (req, res) => {
-
-    if (String(req.url).startsWith('/upload')) {
-
-        const contentType = req.headers['content-type']
-
-        const boundary = `--${contentType.split('boundary=')[1]}`
-
-        console.log({ contentType, boundary })
-
-        let body = Buffer.alloc(0)
-
-        req.on("data", chunk => {
-            body = Buffer.concat([body, chunk])
-        })
-
-        req.on("end", () => {
-            const parts = body.toString('utf8')
-            .split(boundary)
-            .filter(part => part.trim() && part !== "--")
-
-            let fileName = ''
-            let fileData = Buffer.alloc(0)
-            const formFields = {}
-
-            parts.forEach(part => {
-
-                const [headers, contant] = part.split('\r\n\r\n')
-
-                if (headers.includes("filename=")) {
-                    fileName = headers.match(/filename="(.+?)"/)[1]
-                    fileData = Buffer.from(String(contant).trim(), 'binary')
-                } else {
-                    const nameMatch = headers.match(/name="(.+?)"/)
-
-                    if (nameMatch) {
-                        formFields[nameMatch[1]] = String(contant).trim()
-                    }
-                }
-            })
-
-            if (fileName) {
-                const filePath = path.join(__dirname, 'src', 'uploads', fileName)
-                fs.writeFileSync(filePath, fileData)
-            }
-
-            console.log({ formFields })
-
-        })
-
-        res.writeHead(200, {
-            "content-type": "text/plain"
-        })
-        res.end("File uploaded")
-        return 
-    }
-
-    if (String(req.url).startsWith('/static')) {
-        let fileData = staticRoutes(req)
-
-        if (fileData) {
-            res.writeHead(200, {
-                "content-type": "text/html"
-            })
-    
-            res.end(fileData)
-            return
-        }
-    }
-
-    if (String(req.url).startsWith('/api/v1/')) {
-
-        const routes = apiRoutes()
-
-        for(const route of routes) {
-            const match = String(req.url).match(route.pattern)
-
-            if (match) {
-                const data = route.handler(match[1])
-
-                res.writeHead(200, {
-                    "content-type": "application/json"
-                })
-
-                res.end(JSON.stringify(data))
-
-                return
-            }
-        }
-    }
-
-    res.writeHead(404, {
-        "content-type": "text/html"
-    })
-
-    res.end(getNotFoundPage())
+server.post('/', (req, res) => {
+    console.log({ body: req.body })
+    res.send("Hello from Express")
 })
 
-function apiRoutes() {
-    return [
-        { pattern: /^\/api\/v1\/users\/(\d+)$/,  handler: async (id) => getUser(id) },
-        { pattern: /^\/api\/v1\/posts\/(\d+)$/, handler: (id) =>  getPost(id)}
-    ]
-}
-
-// function getUser(id) {
-//     console.log({id})
-//     return {
-//         firstName: "John",
-//         age: 30
-//     }
-// }
-
-function getPost(id) {
-    return {
-        title: "Nodejs",
-    }
-}
-
-
-function staticRoutes(req) {
-    const filePath = `${global.__dirname}${req.url}`
-
-    if (!fs.existsSync(filePath)) {
-        return false
-    }
-
-    if (fs.lstatSync(filePath).isDirectory()) {
-        throw new Error("403 Forbidden action")
-    }
-
-    if (fs.lstatSync(filePath).isFile()) {
-        return fs.readFileSync(filePath)
-    }
-
-    return false
-}
-
-function getNotFoundPage() {
-    const filePath = `${global.__dirname}/static/404.html`
-
-    return fs.readFileSync(filePath)
-}
-
 server.listen(APPP_PORT, () => {
-    logger.info(`HTTP Server is listening on port ${APPP_PORT}`)
+    logger.info(`Express is listening on port ${APPP_PORT}`)
 })
